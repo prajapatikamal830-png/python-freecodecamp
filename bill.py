5d6917de85abfa80c750505a4077db86aedbec7c
kamal prajapati <prajapatikamal830@gmail.com>
2026-03-23 19:22:33 +0530

    Initial commit - python freecodecamp

---
+++ bill.py
@@ -0,0 +1,13 @@
+# build areport card print
+
+name = 'Alice'
+print(name, type(name))
+
+is_student = True
+print(is_student, type(is_student))
+
+age = 20
+print(age, type(age))
+
+score = 80.5
+print(isinstance(score, float))
